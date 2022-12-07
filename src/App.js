05c354228d05d5f43348b773e82a75b5ff141062
paras <parasmanikc@gmail.com>
2022-12-07 13:13:26 +0545

    test 1 files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [redBtnValue, setRedBtnValue] = useState(0)
+  const [blueBtnValue, setBlueBtnValue] = useState(0)
+
+  useEffect(() => {
+    getRemoteData()
+  },[redBtnValue, blueBtnValue])
+
+  const getRemoteData = () => {
+    console.log('fetching remote data')
+  }
+
+  const onRedButtonClick = () => {
+    setRedBtnValue(redBtnValue + 1)
+  }
+
+  const onBlueButtonClick = () => {
+    setBlueBtnValue(blueBtnValue + 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={{ background: 'red'}} onClick={onRedButtonClick}>Value: {redBtnValue}</button>
+      <br />
+      <button style={{ background: 'blue'}} onClick={onBlueButtonClick} >Value: {blueBtnValue}</button>
     </div>
   );
 }
